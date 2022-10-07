c63d6c15adfb7502a954e1677d998472cf36653c
mecarito <pieritomecar@gmail.com>
2022-10-08 02:57:16 +0300

    attempt to fix failing builds

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,7 @@
-import { firebase } from "../Configurations";
-
 export const environment = {
   firebase: {
-    projectId: firebase.projectId,
-    appId: firebase.appId,
-    storageBucket: firebase.storageBucket,
-    apiKey: firebase.apiKey,
-    authDomain: firebase.authDomain,
-    messagingSenderId: firebase.messagingSenderId,
-    measurementId: firebase.measurementId
+    projectId: process.env['projectId'],
+    appId: process.env['appId'],
   },
-  production: true
+  production: true,
 };
