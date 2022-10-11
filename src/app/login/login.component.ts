b3c0ccd887a076ba7fde7d792931b1b1ffeebdf1
mecarito <pieritomecar@gmail.com>
2022-10-12 01:27:59 +0300

    updated login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,24 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 import firebase from 'firebase/compat/app';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  email = new FormControl('');
+  password = new FormControl('');
 
-  constructor(public auth: AngularFireAuth) { 
+  constructor(public auth: AngularFireAuth, public router: Router) {}
 
-  }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   async loginWithGoogle() {
-    const user = await this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
-    console.log( user )
+    const response = await this.auth.signInWithPopup(
+      new firebase.auth.GoogleAuthProvider()
+    );
+
+    if (response.operationType === 'signIn') {
+      this.router.navigate(['dashboard']);
+    }
   }
 
+  async loginWithEmailAndPassword() {
+    const response = await this.auth.signInWithEmailAndPassword(
+      this.email.value as string,
+      this.password.value as string
+    );
+    if (response.operationType === 'signIn') {
+      this.router.navigate(['dashboard']);
+    }
+  }
 }
