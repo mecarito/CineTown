24197ea82c9f2ed276657c1f82ca78fd40ae05da
mecarito <pieritomecar@gmail.com>
2022-10-11 05:55:39 +0300

    added some types

---
+++ src/app/services/movies.services.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Movie, Categories } from '../types';
+
+@Injectable()
+export class MovieService {
+  baseUrl = environment.baseUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getUpcommingMovies() {
+    return this.http.get<Movie>(`${this.baseUrl}movies/upcomming`);
+  }
+}
