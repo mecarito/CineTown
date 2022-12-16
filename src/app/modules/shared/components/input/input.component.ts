4fcb6f6c0ddf0642cd3c2c621ee0f905b23ccf3d
mecarito <pieritomecar@gmail.com>
2022-12-16 13:12:15 +0300

    updated buttons and inputs styles for mobile responsiveness

--- src/app/modules/shared/components/input/input.component.ts
+++ src/app/modules/shared/components/input/input.component.ts
@@ -15,19 +15,19 @@ export class InputComponent implements OnInit {
   visible = false;
 
   validInput =
-    'w-96 py-2 px-4 h-12 ring-1 ring-gray-300 focus:ring-theme-black text-theme-black bg-white rounded-lg text-base';
+    'w-64 md:w-96 py-2 px-4 h-12 ring-1 ring-gray-300 focus:ring-theme-black text-theme-black bg-white rounded-lg text-base';
   invalidInput =
     'w-96 py-2 px-4 h-12 ring-1 ring-theme-red text-theme-black bg-white rounded-lg text-base';
 
   validPassword =
-    'w-96 py-2 px-4 h-12 ring-1 focus-within:ring-2 ring-gray-300 focus-within:ring-theme-black text-theme-black bg-white rounded-lg text-base flex flex-row space-x-2 items-center';
+    'w-64 md:w-96 py-2 px-4 h-12 ring-1 focus-within:ring-2 ring-gray-300 focus-within:ring-theme-black text-theme-black bg-white rounded-lg text-base flex flex-row space-x-2 items-center';
   invalidPassword =
     'w-96 py-2 px-4 h-12 ring-1 focus-within:ring-2 ring-theme-red focus-within:ring-theme-black text-theme-black bg-white rounded-lg text-base flex flex-row space-x-2 items-center';
 
   constructor() {}
 
   ngOnInit(): void {
-    this.valid = true
+    this.valid = true;
   }
 
   onTextChange(value: string) {
