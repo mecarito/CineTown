c208cbe3ba6d05eb6008957ec5a8f0547a2bcfea
mecarito <pieritomecar@gmail.com>
2022-12-16 19:59:20 +0300

    fixed input auto scalling bug on invalid state

--- src/app/modules/shared/components/input/input.component.ts
+++ src/app/modules/shared/components/input/input.component.ts
@@ -17,12 +17,12 @@ export class InputComponent implements OnInit {
   validInput =
     'w-64 md:w-96 py-2 px-4 h-12 ring-1 ring-gray-300 focus:ring-theme-black text-theme-black bg-white rounded-lg text-base';
   invalidInput =
-    'w-96 py-2 px-4 h-12 ring-1 ring-theme-red text-theme-black bg-white rounded-lg text-base';
+    'w-64 md:w-96 py-2 px-4 h-12 ring-1 ring-theme-red text-theme-black bg-white rounded-lg text-base';
 
   validPassword =
     'w-64 md:w-96 py-2 px-4 h-12 ring-1 focus-within:ring-2 ring-gray-300 focus-within:ring-theme-black text-theme-black bg-white rounded-lg text-base flex flex-row space-x-2 items-center';
   invalidPassword =
-    'w-96 py-2 px-4 h-12 ring-1 focus-within:ring-2 ring-theme-red focus-within:ring-theme-black text-theme-black bg-white rounded-lg text-base flex flex-row space-x-2 items-center';
+    'w-64 md:w-96 py-2 px-4 h-12 ring-1 focus-within:ring-2 ring-theme-red focus-within:ring-theme-black text-theme-black bg-white rounded-lg text-base flex flex-row space-x-2 items-center';
 
   constructor() {}
 
